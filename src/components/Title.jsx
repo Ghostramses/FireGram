3b17fdfe152a236cd5ba2e2ada9d8d1afc6fc167
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2021-01-14 20:18:35 -0600

    Finalizado

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,60 @@ const Title = () => {
         <div className='title'>
             <h1>Para la novia más hermosa de todo el universo</h1>
             <h2>Nuestras fotos</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>
+                Mi princesa. Hoy es nuestro tercer aniversario!!!!! Se que el
+                día de hoy no podemos vernos, es una situación que sale de
+                nuestras manos... Pero no quería que este día pasará
+                inadvertido.
+            </p>
+            <p>
+                Para empezar no sabes lo mucho que te amo, me tienes todo menso
+                por ti, me fascina la sensación que causas en mi, sólo tú puedes
+                lograrla... /^\ Haces que saque lo mejor de mi, siempre me
+                apoyas en todo lo que hago y no quiero decepcionarte, me
+                superare las veces necesarias para que siempre estés orgullosa
+                de mi, tu eres aquello que le da color a mi vida.
+            </p>
+            <p>
+                Te he visto crecer desde hace ya casi 5 años!!! Literalmente es
+                casi cuarto de nuestras vidas!!! No tienes idea de cuánto te
+                admiro, eres amable, tierna, cariñosa, adorable, dulce, siempre
+                positiva y disfrutas cada momento de tu vida.
+            </p>
+            <p>
+                Gracias por darme tanto amor, gracias por estar ahí cuando nadie
+                más lo hace, por apoyarme, por amarme, por darme lo mucho o poco
+                que tienes, gracias a tus besos, abrazos y caricias me haces
+                sentir seguro y amado... /^\ Perdón cuando te causo problemas...
+                O cuando te hago sentir mal... Sabes que no es mi intención,
+                muchas gracias por seguir conmigo después de tanto y de verdad
+                espero que podamos seguir por mucho tiempo más!!!
+            </p>
+            <p>
+                Gracias por soportar mi carácter y mi humor, se que no soy la
+                persona más fácil del mundo. Yo sé que lograrás todo lo que te
+                propongas, eres una niña increíble!!!
+            </p>
+            <p>
+                Este día quiero decirte que te amo, y no importa lo que pase
+                siempre será así, yo soy un pingüinito recuerdas? Se que estamos
+                destinados a estar juntos y de verdad lucharé cada segundo para
+                que de verdad suceda...
+            </p>
+            <p>
+                Como una sorpresa, cree esta página para que se convierta en
+                nuestro albúm y así poder guardar las fotos que tenemos juntos,
+                existe un botón para agregar más en cuanto tengamos nuevas,
+                siéntete libre de verlas cariño, ah pero eso sí, te recomiendo
+                que uses el wifi de tu casa, porque no pude lograr reducir el
+                consumo de datos -.-' perdón, y si, por si te lo preguntas, esta
+                es la página en la cual estaba trabajando.
+            </p>
+            <h4>
+                Te amo demasiado <br />
+                Atte: Tu pingüino que te ama mucho >O3O)>
+            </h4>
+            <h3>¡¡¡Feliz 3er aniversario pequeña!!!</h3>
         </div>
     );
 };
